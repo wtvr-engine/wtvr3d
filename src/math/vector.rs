f4c9b045020a902a393c307897302b265964923e
John Tiesselune <tiesse@wtvr.rocks>
2018-02-03 19:41:32 +0100

    Improved vector3 with new functions

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -5,6 +5,7 @@ use std::ops::{Add, Sub, Mul, AddAssign, MulAssign, SubAssign};
 
 /// # Vector3
 /// A simple f32 Vector3 that supports most of vector common operations.
+#[derive(Clone)]
 pub struct Vector3 {
     /// the x coordinate of the vector
     pub x : f32,
@@ -52,7 +53,7 @@ impl Vector3 {
         self.x == 0.0 && self.y == 0.0 && self.z == 0.0
     }
 
-    /// Computes the lenght, or norm, of the vector.
+    /// Computes the length, or norm, of the vector.
     ///
     /// # Examples
     ///
@@ -61,7 +62,21 @@ impl Vector3 {
     /// assert_eq!(v1.length(),11.0_f32.sqrt());
     /// ```
     pub fn length(&self) -> f32{
-        (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
+        self.sq_length().sqrt()
+    }
+
+    /// Computes the squared length, or norm, of the vector.
+    ///
+    /// This is a bit less CPU-intensive than `length()` since it doesn't involve `f32.sqrt()`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let v1 = Vector3{ x : 1.0, y : 1.0, z : 3.0};
+    /// assert_eq!(v1.sq_length(),11.0);
+    /// ```
+    pub fn sq_length(&self) -> f32{
+        (self.x*self.x + self.y*self.y + self.z*self.z)
     }
 
     /// Tests whether the has unit length
@@ -74,6 +89,30 @@ impl Vector3 {
     /// ```
     pub fn normal(&self) -> bool{
         self.length() == 1.0
+    }
+
+    /// Normalizes a vector, to give it unit length.
+    ///
+    /// **Warning**: this function does not guarantee the vector to be of length 1: it only garantees it to be *roughly* one.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let v1 = Vector3 {x : 2.0, y : 0.0, z : 0.0};
+    /// v1.normalize();
+    /// assert_eq!(v1.length(),1.0);
+    /// ```
+    ///
+    /// ```
+    /// let v2 = Vector3 {x : 2.0, y : 3.0, z : -2.0};
+    /// v1.normalize();
+    /// assert!((1.0_f32 - v1.length()).abs() < 0.00001);
+    /// ```
+    pub fn normalize(&mut self){
+        let len = self.length();
+        self.x /= len;
+        self.y /= len;
+        self.z /= len;
     }
 
     /// Computes the dot product (scalar product) of two vectors
@@ -106,6 +145,19 @@ impl Vector3 {
             z : self.x*v.y - self.y*v.x,
         }
     }
+
+    /// Linear interpolation for vectors
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let v1 = Vector3 {x : 1.0, y : 2.0, z : -1.0};
+    /// let v2 = Vector3 {x : 2.0, y : -2.0, z : 3.0};
+    /// let v3 = Vector3::lerp(v1,v2,0.6);
+    /// ```
+    pub fn lerp(v1 : &Vector3, v2 : &Vector3, i : f32) -> Vector3{
+        v1 * (1.0_f32 - i) + v2 * i
+    }
 }
 
 impl Add for Vector3 {
@@ -116,10 +168,26 @@ impl Add for Vector3 {
     }
 }
 
+impl<'a> Add for &'a Vector3 {
+    type Output = Vector3;
+
+    fn add(self, v: &'a Vector3) -> Vector3 {
+        Vector3 {x : self.x + v.x, y : self.y + v.y, z : self.z + v.z}
+    }
+}
+
 impl Sub for Vector3 {
     type Output = Vector3;
 
     fn sub(self, v: Vector3) -> Vector3 {
+        Vector3 {x : self.x - v.x, y: self.y - v.y, z : self.z - v.z}
+    }
+}
+
+impl<'a> Sub for &'a Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, v: &'a Vector3) -> Vector3 {
         Vector3 {x : self.x - v.x, y: self.y - v.y, z : self.z - v.z}
     }
 }
@@ -132,10 +200,26 @@ impl Mul<f32> for Vector3 {
     }
 }
 
+impl<'a> Mul<f32> for &'a Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, f : f32) -> Vector3 {
+        Vector3 {x : self.x * f, y: self.y * f, z : self.z * f}
+    }
+}
+
 impl Mul<Vector3> for f32 {
     type Output = Vector3;
 
     fn mul(self, vec : Vector3) -> Vector3 {
+        Vector3 {x : self * vec.x, y: self * vec.y, z : self * vec.z}
+    }
+}
+
+impl<'a> Mul<&'a Vector3> for f32 {
+    type Output = Vector3;
+
+    fn mul(self, vec : &'a Vector3) -> Vector3 {
         Vector3 {x : self * vec.x, y: self * vec.y, z : self * vec.z}
     }
 }
@@ -200,6 +284,16 @@ mod tests {
     }
 
     #[test]
+    fn sq_length(){
+        let mut vec = Vector3 {x : 1.0, y: 0.0, z : 0.0};
+        assert_eq!(vec.sq_length(),1.0);
+        assert!(vec.normal());
+        vec.y = 4.0;
+        vec.x = 3.0;
+        assert_eq!(vec.sq_length(), 25.0);
+    }
+
+    #[test]
     fn length(){
         let mut vec = Vector3 {x : 1.0, y: 0.0, z : 0.0};
         assert_eq!(vec.length(),1.0);
@@ -207,6 +301,21 @@ mod tests {
         vec.y = 4.0;
         vec.x = 3.0;
         assert_eq!(vec.length(), 25.0_f32.sqrt());
+    }
+
+    #[test]
+    fn normal() {
+        let mut v1 = Vector3 {x : 1.0, y : 0.0, z : 0.0};
+        assert!(v1.normal());
+        v1.x = 2.0;
+        assert!(!v1.normal());
+    }
+
+    #[test]
+    fn normalize() {
+        let mut v1 = Vector3 {x : 2.0, y : 4.0, z : 5.0};
+        v1.normalize();
+        assert!((1.0_f32 - v1.length()).abs() < 0.00001);
     }
 
     #[test]
@@ -222,6 +331,14 @@ mod tests {
         let v2 = Vector3 { x : 0.0, y : 4.0, z : 5.0};
         let result = Vector3{ x : 7.0, y : -5.0, z : 4.0};
         assert!(v1.cross_product(&v2).equals(&result));
+    }
+
+    #[test]
+    fn lerp() {
+        let v1 = Vector3 {x : 1.0, y : 2.0, z : -1.0};
+        let v2 = Vector3 {x : 2.0, y : -2.0, z : 3.0};
+        let result = Vector3 {x : 1.5, y : 0.0, z : 1.0};
+        assert!(Vector3::lerp(&v1,&v2,0.5).equals(&result));
     }
 
     #[test]
