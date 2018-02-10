8e369ed9b67ea6bbbbf0d8f167029f0367351431
John Tiesselune <tiesse@wtvr.rocks>
2018-02-10 12:04:38 +0100

    Added color and improved mod

---
+++ src/math/color.rs
@@ -0,0 +1,9 @@
+//! # Color
+//! Implementation of a color.
+
+struct Color {
+    r : f32,
+    g : f32,
+    b : f32,
+    a : f32,
+}
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,9 +1,19 @@
 //! # math
 //!
 //! A module that provides most of the mathematical requirements to create a full 3d engine, like vectors and matrices along with their common operations.
+pub use self::vector::Vector3;
+
+pub use self::matrix::Matrix4;
+
+pub use self::quaternion::Quaternion;
+
 
 pub mod vector;
 
 pub mod matrix;
 
 pub mod quaternion;
+
+pub mod color;
+
+pub const PI : f32 =  3.14159265359 as f32;
