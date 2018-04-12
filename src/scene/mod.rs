7a44ae782a2a59dc93d146ae40b2d135a52db6d4
John Tiesselune <tiesse@wtvr.rocks>
2018-04-12 12:54:20 +0200

    Added first attempt at scene tree

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,33 +1,76 @@
-use super::math::{Matrix4,Vector3};
-use std::option::Option;
-use std::boxed::Box;
+//! # scene
+//!
+//! A module that implements a tree structure and entity system for the 3D scene
 
-pub struct SceneObject {
-    translation : Vector3,
-    rotation : Vector3,
-    scale : Vector3,
-    dirty : bool,
-    matrix : Matrix4,
-    parent : Option<Box<SceneObject>>,
-    first_child : Option<Box<SceneObject>>,
-    last_child : Option<Box<SceneObject>>,
-    next_sibling : Option<Box<SceneObject>>,
-    previous_sibling : Option<Box<SceneObject>>
+pub use self::transform::{Transform, TransformId};
+pub use self::component::{Component, ComponentId};
+use super::math::Vector3;
+use std::mem;
+use std::vec::Vec;
+use std::boxed::Box;
+use std::collections::HashMap;
+
+pub mod transform;
+
+pub mod component;
+
+/// # Scene
+/// Scene is an Arena containing a tree. It is the owner of all the children Transforms and components.
+pub struct Scene {
+    transforms : Vec<Transform>,
+    free_transforms : Vec<TransformId>,
+    components : HashMap<TransformId,Box<Component>>
 }
 
-impl SceneObject{
-    pub fn new(t: Vector3, r : Vector3, s : Vector3) -> SceneObject {
-         SceneObject {
-            translation : t,
-            rotation : r,
-            scale : s,
-            dirty : true,
-            matrix : Matrix4::identity(),
-            parent : None,
-            first_child : None,
-            last_child : None,
-            next_sibling : None,
-            previous_sibling : None
+
+/// Creates a new empty scene. Usually, you will have one unique scene.
+///
+/// # Examples
+///
+/// ```
+/// let scene = Scene::new();
+/// ```
+impl Scene {
+    pub fn new() -> Scene {
+        Scene {
+            transforms : Vec::new(),
+            free_transforms : Vec::new(),
+            components : HashMap::new()
         }
     }
+
+    pub fn get(&self, tid : TransformId) -> &Transform {
+        &self.transforms[tid.index]
+    }
+
+    pub fn append_new(&mut self, parent : Option<TransformId>) -> TransformId {
+        let mut t = Transform::new(Vector3::zero(),Vector3::zero(),Vector3 { x: 1.0, y : 1.0, z : 1.0});
+        let mut result = TransformId {index : self.transforms.len() };
+        if !self.free_transforms.is_empty() {
+            let i = self.free_transforms[0];
+            mem::replace(&mut self.transforms[i.index],t);
+            self.free_transforms.remove(0);
+            result = i
+        }
+        else{
+            self.transforms.push(t);
+        }
+        t.parent = parent;
+        if let Some(parentId) = parent {
+            let last_child : Option<TransformId>;
+            {
+                let parentTransform = self.get(parentId);
+                t.previous_sibling = parentTransform.last_child;
+                parentTransform.last_child = Some(result);
+                last_child = t.previous_sibling
+            }
+            if let Some(lchild) = last_child {
+                self.get(lchild).next_sibling = Some(result);
+            }
+
+        }
+        result
+    }
+
+
 }
---
+++ src/scene/transform.rs
@@ -0,0 +1,43 @@
+use super::super::math::{Matrix4,Vector3};
+use std::option::Option;
+
+/// # Transform
+/// A Transform is a node in the Scene tree. It allows moving into the tree in any direction.
+pub struct Transform<'a> {
+    translation : Vector3,
+    rotation : Vector3,
+    scale : Vector3,
+    dirty : bool,
+    enabled : bool,
+    dead : bool,
+    matrix : Matrix4,
+    pub parent : Option<TransformId>,
+    pub first_child : Option<TransformId>,
+    pub last_child : Option<TransformId>,
+    pub next_sibling : Option<TransformId>,
+    pub previous_sibling : Option<TransformId>
+}
+
+#[derive(Hash,Copy, Clone, PartialEq, Eq)]
+pub struct TransformId {
+    pub index : usize
+}
+
+impl Transform {
+    pub fn new(t: Vector3, r : Vector3, s : Vector3) -> Transform {
+         Transform {
+            translation : t,
+            rotation : r,
+            scale : s,
+            dirty : true,
+            enabled : true,
+            dead: false,
+            matrix : Matrix4::identity(),
+            parent : None,
+            first_child : None,
+            last_child : None,
+            next_sibling : None,
+            previous_sibling : None
+        }
+    }
+}
