20d566c6e0223bd1dd940b09c9a95a26aca3ddbb
John Tiesselune <tiesse@wtvr.rocks>
2019-10-26 21:41:46 +0200

    Documented Buffer

--- src/renderer/buffer.rs
+++ src/renderer/buffer.rs
@@ -1,24 +1,45 @@
 //! # Buffer
 //!
-//! Interface and implementations for managing Buffers
+//! Interface and implementations for managing Buffers and Attributes.
 
 use super::shader_data_type::ShaderDataType;
 use js_sys::{Float32Array, Int16Array, Uint8Array};
 use std::rc::Rc;
 use web_sys::{WebGlBuffer, WebGlProgram, WebGlRenderingContext};
 
+/// ## Buffer
+/// 
+/// A `Buffer` reprensents information about an attribute and its buffer.  
+/// It contains its attribute's name, location, buffer value, as well as
+/// its data type and buffer offset.
 #[derive(Clone)]
 pub struct Buffer {
+
+    /// Attribute name for the current Buffer.
     attribute_name: String,
+
+    /// Automatically computed attribute location for use in WebGL.
     attribute_location: Option<i32>,
+
+    /// Buffer reference; several `Buffer` objects can use the same `WebGlBuffer`
     value: Rc<WebGlBuffer>,
+
+    /// Data type in the shader as defined in `ShaderDataType`
     data_type: ShaderDataType,
+
+    /// Numeric type (automatically set); can be Float32, Int16, and UInt8.
     number_type: u32,
+
+    /// Custom stride to be used when setting the attribute pointer
     pub stride: i32,
+
+    /// Offset in the giver buffer for the attribute pointer.
     pub offset: i32,
 }
 
 impl Buffer {
+
+    /// Constructor using a instance of `Float32Array` and an attribute name
     pub fn from_f32_data(
         context: &WebGlRenderingContext,
         name: &str,
@@ -45,6 +66,7 @@ impl Buffer {
         }
     }
 
+    /// Constructor using a instance of `Int16Array` and an attribute name
     pub fn from_i16_data(
         context: &WebGlRenderingContext,
         name: &str,
@@ -71,6 +93,7 @@ impl Buffer {
         }
     }
 
+    /// Constructor using a instance of `Uint8Array` and an attribute name
     pub fn from_u8_data(
         context: &WebGlRenderingContext,
         name: &str,
@@ -97,6 +120,8 @@ impl Buffer {
         }
     }
 
+    /// Function that looks up the attribute location associated with this `Buffer`.  
+    /// To be used at initialization time, before any rendering occurs.
     pub fn lookup_location(
         &mut self,
         context: &WebGlRenderingContext,
@@ -108,6 +133,8 @@ impl Buffer {
         }
     }
 
+    /// Enables and sets the attribute pointer at the context level.  
+    /// Meant to be called just before rendering.
     pub fn enable_and_bind_attribute(&self, context: &WebGlRenderingContext) {
         context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&self.value));
         let location = self.attribute_location.unwrap() as u32;
