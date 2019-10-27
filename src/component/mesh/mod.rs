b1a6699b155ef56b6d89f3f8c23774b85a868332
John Tiesselune <tiesse@wtvr.rocks>
2019-10-27 12:27:44 +0100

    Documented Mesh

--- src/component/mesh/mod.rs
+++ src/component/mesh/mod.rs
@@ -1,4 +1,3 @@
-//! # mesh
 //! Representation of a mesh in a scene
 
 pub mod mesh_data;
@@ -8,26 +7,39 @@ use crate::renderer::material::MaterialInstance;
 pub use mesh_data::MeshData;
 use web_sys::WebGlRenderingContext;
 
+/// Mesh component for an entity in the 3D scene.  
+/// Links some `MeshData` to some `MaterialInstance`.
 pub struct Mesh<'a> {
+
+    /// `MeshData` in use for this mesh, containing the vertex data.
     data: MeshData,
+
+    /// `MaterialInstance` to use to render this mesh in the 3d scene
     pub material: MaterialInstance<'a>,
 }
 
 impl<'a> Mesh<'a> {
+
+    /// Constructor. Uses a `MeshData` instance and a `MaterialInstance`
     pub fn new(data: MeshData, material: MaterialInstance<'a>) -> Mesh<'a> {
         Mesh {
             data: data,
             material: material,
         }
     }
+
+    /// Returns a slice of the associated `WebGlProgram` buffers
     pub fn get_buffers(&self) -> &[Buffer] {
         self.data.get_buffers()
     }
 
+    /// Returns the number of vertices in this `Mesh`e's `Buffer`s.
     pub fn get_vertex_count(&self) -> i32 {
         self.data.get_vertex_count()
     }
 
+    /// Lookup for this Mesh attributes locations, going though every underlying buffer.  
+    /// Should be done at initialization time before any rendering starts.
     pub fn lookup_locations(&mut self, context: &WebGlRenderingContext) -> () {
         self.data
             .lookup_locations(context, self.material.get_parent().borrow().get_program());
--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,3 +1,5 @@
+//! Components that are attached to entities in the 3D scene.
+
 pub mod camera;
 pub mod mesh;
 pub mod transform;
