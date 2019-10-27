10b7ccda5d27b8414e89adfbb23aa44f3cb6f801
John Tiesselune <tiesse@wtvr.rocks>
2019-10-27 20:49:28 +0100

    Documentation + some util conversion functions

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -9,14 +9,21 @@ use specs::{Builder, World, WorldExt};
 use wasm_bindgen::prelude::*;
 use web_sys::{HtmlCanvasElement, WebGlRenderingContext};
 
+/// Scene representation, to be shared with JS.
+/// A scene holds a renderer and a `specs` world.
 #[wasm_bindgen]
 pub struct Scene {
+    /// The main renderer for the scene  
+    /// Is None by default, before being initialized with a Camera.
     main_renderer: Option<Renderer>,
+
+    /// The current `specs` World for this scene.
     world: World,
 }
 
 #[wasm_bindgen]
 impl Scene {
+    /// Constructor. Initializes a new `Scene` with a fresh world and registers common components.
     #[wasm_bindgen(constructor)]
     pub fn new() -> Scene {
         let world = World::new();
@@ -28,6 +35,7 @@ impl Scene {
         scene
     }
 
+    /// Creates an entity holding a Camera. Returns its Entity ID.
     pub fn create_camera_entity(
         &mut self,
         aspect_ratio: f32,
@@ -42,8 +50,8 @@ impl Scene {
             fov,
             znear,
             zfar,
-            &Point3::new(position.x, position.y, position.z),
-            &Point3::new(target.x, target.y, target.z),
+            &position.to_point3(),
+            &target.to_point3(),
         );
         let entity = self.world.create_entity().with(camera).build();
         entity.id()
@@ -51,6 +59,7 @@ impl Scene {
 }
 
 impl Scene {
+    /// Registers every common component for the current world.
     fn register_components(&mut self) -> () {
         self.world.register::<Transform>();
         self.world.register::<TransformParent>();
--- src/utils/transfer_types.rs
+++ src/utils/transfer_types.rs
@@ -1,17 +1,37 @@
+use nalgebra::{Point3, Vector3};
 /// Defines a few transfer types to facilitate communciation between JS world and WASM world.
 use wasm_bindgen::prelude::*;
 
+/// Simple transfer type for Vector3 since it is not `wasm-bindgen` compatible.
 #[wasm_bindgen]
 pub struct Vector3Data {
+    /// x coordinate
     pub x: f32,
+
+    /// y coordinate
     pub y: f32,
+
+    /// z coordinate
     pub z: f32,
 }
 
 #[wasm_bindgen]
 impl Vector3Data {
+    /// Constructor: creates a new VectorData from 3 coordinates.
     #[wasm_bindgen(constructor)]
     pub fn new(x: f32, y: f32, z: f32) -> Vector3Data {
         Vector3Data { x: x, y: y, z: z }
+    }
+}
+
+impl Vector3Data {
+    /// Quick conversion to `nalgebra`'s Point3
+    pub fn to_point3(&self) -> Point3<f32> {
+        Point3::new(self.x, self.y, self.z)
+    }
+
+    /// Quick conversion to `nalgebra`'s Point3
+    pub fn to_vector3(&self) -> Vector3<f32> {
+        Vector3::new(self.x, self.y, self.z)
     }
 }
