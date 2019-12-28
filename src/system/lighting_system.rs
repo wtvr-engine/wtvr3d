43eca04c3f6bf126545eb0883c80eda4e3618a0c
John Tiesselune <tiesse@wtvr.rocks>
2019-12-28 14:26:51 +0100

    Computed the average ambiant light instead

--- src/system/lighting_system.rs
+++ src/system/lighting_system.rs
@@ -1,13 +1,13 @@
 //! System for registering lights before rendering
 
 use crate::component::{Light,Direction,Transform,Cone,Enabled};
-use nalgebra::Matrix4;
+use nalgebra::{Matrix4,Vector3};
 use specs::{System,Write,ReadStorage, Join,Entities};
 
 /// Resource for sharing light information between the light system and the rendering system
 #[derive(Default)]
 pub struct LightRepository {
-    pub ambiant : Vec<Light>,
+    pub ambiant : Option<Light>,
     pub directional : Vec<(Light,Direction)>,
     pub point : Vec<(Light,Matrix4<f32>)>,
     pub spot : Vec<(Light,Matrix4<f32>,Direction,Cone)>,
@@ -26,10 +26,12 @@ impl<'a> System<'a> for LightingSystem {
         Write<'a, LightRepository>,
     );
     fn run(&mut self, (entities, lights, transforms, directions, cones, enableds, mut light_repository): Self::SystemData) {
-        light_repository.ambiant.clear();
+        light_repository.ambiant = None;
         light_repository.directional.clear();
         light_repository.point.clear();
         light_repository.spot.clear();
+        let mut ambiant = Light { color : Vector3::new(0.0,0.0,0.0), intensity : 0.0};
+        let mut some_ambiant = false;
         for (entity,light, _) in (&entities, &lights, &enableds).join() {
             let direction_opt = directions.get(entity);
             let transform_opt = transforms.get(entity);
@@ -44,8 +46,14 @@ impl<'a> System<'a> for LightingSystem {
                 light_repository.spot.push((light.clone(),transform.get_world_matrix(),direction.clone(),cone.clone()));
             }
             else if let (None,None,None) = (transform_opt,cone_opt,direction_opt) {
-                light_repository.ambiant.push(light.clone());
+                //light_repository.ambiant.push(light.clone());
+                some_ambiant = true;
+                ambiant.color = ambiant.color * ambiant.intensity + light.color * light.intensity;
+                ambiant.intensity = ambiant.intensity + light.intensity;
             }
+        }
+        if some_ambiant {
+            light_repository.ambiant = Some(ambiant);
         }
     }
 }
