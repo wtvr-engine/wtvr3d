c185648093dcf8d979533a4e93ac918e9a9a8153
John Tiesselune <tiesse@wtvr.rocks>
2020-01-02 02:09:23 +0100

    Updated the test again, cleaner version

--- demo/assets/materials/shaders/uniform_color.frag.glsl
+++ demo/assets/materials/shaders/uniform_color.frag.glsl
@@ -4,5 +4,5 @@ uniform sampler2D u_tex_diffuse;
 varying vec2 v_tex_coordinates;
 
 void main() {
-    gl_FragColor = texture2D(u_tex_diffuse, v_tex_coordinates);;
+    gl_FragColor = texture2D(u_tex_diffuse, vec2(v_tex_coordinates.x, 1.0 - v_tex_coordinates.y));;
 }
