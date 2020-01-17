7147ecea33ace97fde6d80dd542c17f7375e2ed5
John Tiesselune <tiesse@wtvr.rocks>
2020-01-17 14:23:48 +0100

    Initial demo

---
+++ sw.js
@@ -0,0 +1,51 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "296793d04035eab8235974c65717fd07"
+  },
+  {
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0"
+  },
+  {
+    "url": "polyfills/dynamic-import.b745cfc9384367cc18b42bbef2bbdcd9.js",
+    "revision": "ed55766050be285197b8f511eacedb62"
+  },
+  {
+    "url": "polyfills/webcomponents.dae9f79d9d6992b6582e204c3dd953d3.js",
+    "revision": "fe4a22f36087db029cd3f476a1935410"
+  },
+  {
+    "url": "wtvr3d-demo-b7ec685f.js",
+    "revision": "72919fb6bb6cf403152d6b8e64fac1f6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
